84c38aa2795520a0a84796273b1ab257196c7e71
kenta-wakasa <wakasa.kenta.pg@gmail.com>
2021-01-11 18:55:42 +0900

    関数位置の調整。

--- lib/ui/stopwatch/stopwatch_controller.dart
+++ lib/ui/stopwatch/stopwatch_controller.dart
@@ -33,19 +33,19 @@ class StopwatchController extends ChangeNotifier {
     timer?.cancel();
   }
 
-  void start() {
-    isWorking = true;
-    countStreamController = StreamController<int>();
-    timer = Timer.periodic(const Duration(milliseconds: 10), _tick);
-    notifyListeners();
-  }
-
   void _tick(Timer timer) {
     _count++;
     countStreamController.add(_count);
     time10msec = (_count % 100).toString().padLeft(2, '0');
     timeSec = ((_count ~/ 100) % 60).toString().padLeft(2, '0');
     timeMin = ((_count ~/ 100) ~/ 60).toString().padLeft(2, '0');
+  }
+
+  void start() {
+    isWorking = true;
+    countStreamController = StreamController<int>();
+    timer = Timer.periodic(const Duration(milliseconds: 10), _tick);
+    notifyListeners();
   }
 
   void pause() {
