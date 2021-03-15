9880758261be7e0e17155cf724b4da205d0fb41f
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2021-03-15 16:16:53 +0900

    変数名の変更と参考Urlの追加

--- lib/ui/jwt/jwt_provider.dart
+++ lib/ui/jwt/jwt_provider.dart
@@ -17,12 +17,13 @@ class JwtProvider extends ChangeNotifier {
     final random = Random.secure();
 
     // key の生成方法はこれであっているのか。
-    final values = List<int>.generate(
+    // 参考:https://www.scottbrady91.com/Dart/Generating-a-Crypto-Random-String-in-Dart
+    final key256bit = List<int>.generate(
       32,
       (i) => random.nextInt(256), // from 0 up to 255 included
     );
-    final key = base64UrlEncode(values).split('=').first;
-    print(key);
+    final k = base64UrlEncode(key256bit).split('=').first;
+    print(k);
     // final sampleKey = 'iFWK6fWdcTlgnzuFn0g04CWUsRaq71gGhRM8wTRzyDj';
 
     final bytes = utf8.encode(
@@ -30,8 +31,7 @@ class JwtProvider extends ChangeNotifier {
       '.'
       'eyJkb2N1bWVudHMiOlt7InRlbXBsYXRlIjoiaHR0cHM6Ly90ZW1wbGF0ZXMuZXhhbXBsZS5jb20vdGVtcGxhdGVzL0R4ckpjNEZoa3ludHV1bWQ4b2MvNFE5Um9jZnpzQjlja1lkWitLSnlEOTZ5RGcvbnM1ZG55azR2RXJuWU9zV3gvaW5kZXguanNvbiIsInBhZ2VzIjpbeyJwYWdlLW51bWJlciI6MSwic3Ryb2tlcyI6IkRnVmJ1aEVockd2SW0xRmwwT3JobzNKQWk3aWcwMGVuTzNOclkraXZaNGR4U2w4eFdqN3hMTzN1MnNLQjBwdmMifV19XX0',
     );
-    // final hmacSha256 = Hmac(sha256, utf8.encode(sampleKey));
-    final hmacSha256 = Hmac(sha256, utf8.encode(key));
+    final hmacSha256 = Hmac(sha256, utf8.encode(k));
     final digest = hmacSha256.convert(bytes);
 
     final base64EUrlEncode = base64UrlEncode(digest.bytes).split('=').first;
